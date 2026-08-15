055514882089af26033c9ea54a803024ab31e10a
Tushar Tewatia <tushar9650ch@gmail.com>
2026-08-15 07:20:25 +0530

    Upload Food Ecommerce Code

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food</h2>
+            <p>Choose your meal </p>
+            <button>View menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
